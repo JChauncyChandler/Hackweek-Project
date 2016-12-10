30d51f6b9dc7077631f1709c9eac926be9de84a1
JChauncyChandler <jackson.c.chandler@gmail.com>
2016-12-10 15:58:07 -0600

    dfg

--- Scripts.php
+++ Scripts.php
@@ -1,18 +1,26 @@
 <?php
-    function conPOST(){
-        $servername = "web2.c7mrwyr3et3m.us-west-2.rds.amazonaws.com,1433";
-        $username = "JacksonChandler";
-        $password = "jackson11";
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
+    if (isset($_POST['id', 'score'])) {
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        echo "Connected successfully";
+        conPOST($_POST['id', 'score']);
+        
+        
+        function conPOST($Id, $Score){
+            $servername = "web2.c7mrwyr3et3m.us-west-2.rds.amazonaws.com,1433";
+            $username = "JacksonChandler";
+            $password = "jackson11";
 
-        $query = "INSERT INTO HIGH_SCORE_TABLE"
+            // Create connection
+            $conn = new mysqli($servername, $username, $password);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+            echo "Connected successfully";
+
+            $query = "INSERT INTO HIGH_SCORE_TABLE (Id, Score) VALUES ('PlayerID', 'PlayerScore')"
+        }
     }
+    
 ?>
